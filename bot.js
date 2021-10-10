9a4c5df5c49d49a1ff5d1d5f4345a673594d2ef1
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2021-10-10 19:53:48 -0400

    Add twitter account API keys

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ var Twit = require('twit'); // import twit package
 // Create new twit object, storing secret values associated with a twitter bot
 // various things liek consumer key, secret key, are for handling users with 
 // different account credentials
+
 var T = new Twit({
   consumer_key:         'kmlD6fbbb3T2PLXi42ORVvPDM',
   consumer_secret:      'QpzTM7GrZPCzRGvUmdTAol05QxzCpZ3exYhPonob8D8cEOeYNe',
@@ -13,3 +14,4 @@ var T = new Twit({
   // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   // strictSSL:            true,     // optional - requires SSL certificates to be valid.
 })
+
