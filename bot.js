6ff5f05ae81ea534310f75ec4a2048ef7ec1c8ad
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2021-10-10 23:43:54 -0400

    Implemented first post and get functions

--- bot.js
+++ bot.js
@@ -10,3 +10,35 @@ var Twit = require('twit'); // import twit package
 var config = require('./config') // returns "{key1: ..., key2: ..., etc.}"
 var T = new Twit(config)
 
+
+// get, post, stream, last one is constantly connected, and can respond to 
+// stuff like mentions
+
+// var params = { q: 'trump since:2011-07-11', count: 2 }
+
+// T.get('search/tweets', params, (err, data, response) => {
+// 	var tweets = data.statuses;
+// 	for (var i = 0; i < tweets.length; i++) {
+// 		console.log(tweets[i].text);
+// 	}
+// })
+
+// Relevant test post
+var tweet = {
+	status: 'Marvel Shwodown Test Tweet 1'
+}
+
+T.post('statuses/update', tweet, tweeted);
+
+function tweeted(err, data, response){
+	if(err){
+		console.log(err);
+	} else{
+		console.log("SUCCESS");
+	}
+}
+
+// Test post
+// T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+//   console.log(data)
+// })
