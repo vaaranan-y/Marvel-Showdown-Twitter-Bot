ffbfe350a8ee0bb045eb09eac7674178c74cafbe
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2021-10-10 23:43:25 -0400

    Stored api access data in config package

---
+++ config.js
@@ -0,0 +1,10 @@
+// Another package we create to store our keys
+
+module.exports = {
+	consumer_key:         'sqJ17LIs8gAhFwedBd74XtWYs',
+  	consumer_secret:      'ACdn78fmai68uBEQ0oHVY4eOc26zvuOMNbX3QpbpLLjwyCiYY8',
+  	access_token:         '1443067983904100354-aQ4qntYqKr0EcXI1vlUoOSmoMvfav5',
+  	access_token_secret:  'WDI5UN2EaMmhCHBemELlYNx1iNnLEDYzPAfG2CHyQpLJw',
+  	// timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  	// strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
